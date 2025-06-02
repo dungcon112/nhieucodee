ab3f8528688100343c8cd2b889f3f382b2279aa5
dungcon112 <dungcon12456789@gmail.com>
2025-06-02 21:45:20 +0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,15 @@ import Index from "@/pages";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 import Verify from "@/pages/verify";
-import Shopee from '@/pages/shopee';
 import { Route, Routes } from "react-router";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Shopee/>} />
-      <Route path='/live' element={<Layout />}>
+      <Route element={<Layout />}>
         <Route index element={<Index />} />
-        <Route path="home" element={<Home />} />
-        <Route path="verify" element={<Verify />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/verify" element={<Verify />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
